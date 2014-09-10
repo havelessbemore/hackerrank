24d35daf8c57e26c24d235534b90cbb14c1c4335
Michael Rojas <havelessbemore@users.noreply.github.com>
2014-09-09 22:58:27 -0400

    Update and rename algorithms/math/meeting-point.java to algorithms/geometry/meeting-point.java

--- algorithms/geometry/meeting-point.java
+++ algorithms/geometry/meeting-point.java
@@ -3,9 +3,11 @@ import java.io.*;
 import java.math.*;
 
 public class Solution {
+    
     public static void main(String[] args) throws IOException {
+        
+      	//INPUT
       	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-      	//Input
       	int N = Integer.parseInt(br.readLine());
       	int[] X = new int[N];
       	int[] Y = new int[N];
@@ -14,9 +16,12 @@ public class Solution {
           	X[i] = Integer.parseInt(temp[0]);
           	Y[i] = Integer.parseInt(temp[1]);
         }
+        
+        //SOLVE
       	//Find center point
       	double cX = avg(X);
       	double cY = avg(Y);
+      	
       	//Snap to nearest house (Euclidean distance)
       	int minI = -1;
       	double minD = Double.MAX_VALUE;
@@ -27,14 +32,17 @@ public class Solution {
               	minI = i;
             }
         }
+        
       	//Sum distances (Grid distances)
       	long sum = 0;
       	for(int i = 0; i < N; ++i){
           	sum += (long)gridDistance(X[i], Y[i], X[minI], Y[minI]);
         }
-      	//Output
-      	System.out.println(sum);
+        
+      	//OUTPUT
+      	System.out.print(sum);
     }
+    
   	private static double avg(int[] ar){
       	long A = 0;
       	for(int v : ar){
@@ -42,9 +50,11 @@ public class Solution {
         }
       	return ((double)A)/ar.length;
     }
+    
   	private static double gridDistance(double x1, double y1, double x2, double y2){
       	return Math.max(Math.abs(x2-x1), Math.abs(y2-y1));
     }
+    
   	private static double euclideanDistance(double x1, double y1, double x2, double y2){
       	return Math.sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1,2));
     }
