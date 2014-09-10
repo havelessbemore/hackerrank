89deedb79b9d82f41d5ce5054dce69a0895768dc
Michael Rojas <havelessbemore@users.noreply.github.com>
2014-09-09 22:55:13 -0400

    Update and rename algorithms/math/find-point.java to algorithms/geometry/find-point.java

--- algorithms/geometry/find-point.java
+++ algorithms/geometry/find-point.java
@@ -2,19 +2,26 @@
 import java.io.*;
 
 public class Solution {
+    
     public static void main(String[] args) throws IOException {
         StringBuffer sb = new StringBuffer();
+        
+        //INPUT
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         for(int T = Integer.parseInt(br.readLine()); T > 0; --T){
             String[] temp = br.readLine().split(" ");
-            int px = Integer.parseInt(temp[0]);
-            int py = Integer.parseInt(temp[1]);
-            int qx = Integer.parseInt(temp[2]);
-            int qy = Integer.parseInt(temp[3]);
-            int dx = qx - px;
-            int dy = qy - py;
-            int rx = qx + dx;
-            int ry = qy + dy;
+            final int px = Integer.parseInt(temp[0]);
+            final int py = Integer.parseInt(temp[1]);
+            final int qx = Integer.parseInt(temp[2]);
+            final int qy = Integer.parseInt(temp[3]);
+            
+            //SOLVE
+            final int dx = qx - px;
+            final int dy = qy - py;
+            final int rx = qx + dx;
+            final int ry = qy + dy;
+            
+            //OUTPUT
             sb.append(rx + " " + ry + "\n");
         }
         System.out.print(sb);
