cc0c4124e80cabaaae4f3e1995036639fd199851
Michael Rojas <havelessbemore@users.noreply.github.com>
2016-02-10 10:56:53 -0500

    Update and-product.java

--- algorithms/bit_manipulation/and-product.java
+++ algorithms/bit_manipulation/and-product.java
@@ -5,19 +5,19 @@ public class Solution {
   public static void main(String[] args) throws IOException {
     StringBuffer sb = new StringBuffer();
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+    
     //For each test
     for(short T = Short.parseShort(br.readLine()); T > 0; --T){
-
+      
       //Get input
       String[] line = br.readLine().split(" ");
       final long A = Long.parseLong(line[0]);
       final long B = Long.parseLong(line[1]);
       
       //Get output
-      long C = B - A;
-      for(long t = C; (t = C & (C-1)) > 0; C = t){}
-      C = A & B & -(C << 1);
+      long C = 1L;
+      for(long D = B - A; C <= D; C <<= 1L){}
+      C = A & B & -C;
       
       //Print output
       sb.append(C).append("\n");
