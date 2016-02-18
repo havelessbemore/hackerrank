6f1ca59a36287ed1073a3266f9f507b7629fcd37
Michael Rojas <havelessbemore@users.noreply.github.com>
2016-02-18 18:53:28 -0500

    Create matching-range-of-characters.java

---
+++ regex/character_class/matching-range-of-characters.java
@@ -0,0 +1,7 @@
+//https://www.hackerrank.com/challenges/matching-range-of-characters
+public class Solution {    
+  public static void main(String[] args) {
+    Regex_Test tester = new Regex_Test();
+    tester.checker("^[a-z][1-9][^a-z][^A-Z][A-Z]");
+  }
+}
