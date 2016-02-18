e43fa919af8ae1d97195290991ba5569aeaa3c3b
Michael Rojas <havelessbemore@users.noreply.github.com>
2016-02-18 18:48:29 -0500

    Create matching-specific-characters.java

---
+++ regex/character_class/matching-specific-characters.java
@@ -0,0 +1,7 @@
+//https://www.hackerrank.com/challenges/matching-specific-characters
+public class Solution {    
+  public static void main(String[] args) {
+    Regex_Test tester = new Regex_Test();
+    tester.checker("^[123][120][xs0][30Aa][xsu][.,]$");
+  }
+}
