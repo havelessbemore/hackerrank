264fdc361f44106f535d5606ac38c166073b6f05
Michael Rojas <havelessbemore@users.noreply.github.com>
2016-02-18 18:57:18 -0500

    Create matching-x-repetitions.java

---
+++ regex/repetitions/matching-x-repetitions.java
@@ -0,0 +1,7 @@
+//https://www.hackerrank.com/challenges/matching-x-repetitions
+public class Solution {    
+  public static void main(String[] args) {
+    Regex_Test tester = new Regex_Test();
+    tester.checker("^[a-zA-Z02468]{40}[\\s13579]{5}$");
+  }
+}
