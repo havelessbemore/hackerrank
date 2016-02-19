b9b7327d376669f6ad63594eb85f5bd118441b87
Michael Rojas <havelessbemore@users.noreply.github.com>
2016-02-18 21:15:11 -0500

    Create positive-lookahead.java

---
+++ regex/assertions/positive-lookahead.java
@@ -0,0 +1,7 @@
+//https://www.hackerrank.com/challenges/positive-lookahead
+public class Solution {
+  public static void main(String[] args) {
+    Regex_Test tester = new Regex_Test();
+    tester.checker("o(?=oo)");
+  }
+}
