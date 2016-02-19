3b5c735fa65f3e6ac9ac6fefb18cb8d6096927ea
Michael Rojas <havelessbemore@users.noreply.github.com>
2016-02-18 19:04:46 -0500

    Create matching-ending-items.java

---
+++ regex/repetitions/matching-ending-items.java
@@ -0,0 +1,7 @@
+//https://www.hackerrank.com/challenges/matching-ending-items
+public class Solution {    
+  public static void main(String[] args) {
+    Regex_Test tester = new Regex_Test();
+    tester.checker("^[a-zA-Z]*s$");
+  }
+}
