21605774573ddeab2746969eef1bc3f13be87e16
Michael Rojas <havelessbemore@users.noreply.github.com>
2016-02-18 19:11:05 -0500

    Create capturing-non-capturing-groups.java

---
+++ regex/grouping_and_capturing/capturing-non-capturing-groups.java
@@ -0,0 +1,7 @@
+//https://www.hackerrank.com/challenges/capturing-non-capturing-groups
+public class Solution {    
+  public static void main(String[] args) {
+    Regex_Test tester = new Regex_Test();
+    tester.checker("(?:ok){3,}");
+  }
+}
