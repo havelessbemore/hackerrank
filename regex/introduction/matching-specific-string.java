2a7d42eb144fb9eacf3a6d6f6b0dcad34a80186b
Michael Rojas <havelessbemore@users.noreply.github.com>
2016-02-18 21:25:18 -0500

    Create matching-specific-string.java

---
+++ regex/introduction/matching-specific-string.java
@@ -0,0 +1,7 @@
+//https://www.hackerrank.com/challenges/matching-specific-string
+public class Solution {    
+    public static void main(String[] args) {
+        Regex_Test tester = new Regex_Test();
+        tester.checker("hackerrank"); 
+    }
+}
