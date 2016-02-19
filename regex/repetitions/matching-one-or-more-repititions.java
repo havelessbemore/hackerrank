bfe543dce703ad5a3da57662c04a2731d2e136a3
Michael Rojas <havelessbemore@users.noreply.github.com>
2016-02-18 19:03:05 -0500

    Create matching-one-or-more-repititions.java

---
+++ regex/repetitions/matching-one-or-more-repititions.java
@@ -0,0 +1,7 @@
+//https://www.hackerrank.com/challenges/matching-one-or-more-repititions
+public class Solution {    
+  public static void main(String[] args) {
+    Regex_Test tester = new Regex_Test();
+    tester.checker("^\\d+[A-Z]+[a-z]+$");
+  }
+}
