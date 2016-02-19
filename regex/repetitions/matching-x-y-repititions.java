8ebadf45cac23d6f4fc2c87170eee3bab31829b2
Michael Rojas <havelessbemore@users.noreply.github.com>
2016-02-18 19:00:15 -0500

    Create matching-x-y-repititions.java

---
+++ regex/repetitions/matching-x-y-repititions.java
@@ -0,0 +1,7 @@
+//https://www.hackerrank.com/challenges/matching-x-y-repititions
+public class Solution {    
+  public static void main(String[] args) {
+    Regex_Test tester = new Regex_Test();
+    tester.checker("^\\d{1,2}[a-zA-Z]{3,}\\.{0,3}$");
+  }
+}
