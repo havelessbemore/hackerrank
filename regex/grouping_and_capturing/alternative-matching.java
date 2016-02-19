7aa36694bdfcf46f146efd842c6fcf8cb041aee0
Michael Rojas <havelessbemore@users.noreply.github.com>
2016-02-18 19:15:26 -0500

    Create alternative-matching.java

---
+++ regex/grouping_and_capturing/alternative-matching.java
@@ -0,0 +1,7 @@
+//https://www.hackerrank.com/challenges/alternative-matching
+public class Solution {    
+  public static void main(String[] args) {
+    Regex_Test tester = new Regex_Test();
+    tester.checker("^(?:Mr|Mr?s|Dr|Er)\\.[a-zA-Z]+$");
+  }
+}
