1b73277d7a2c9e663b2ae5432cb470c0dee4c554
Michael Rojas <havelessbemore@users.noreply.github.com>
2016-03-07 08:30:26 -0500

    Update manasa-and-stones.java

--- algorithms/warmup/manasa-and-stones.java
+++ algorithms/warmup/manasa-and-stones.java
@@ -3,34 +3,37 @@
 import java.io.*;
 
 public class Solution{
-    
     public static void main(String[] args) throws IOException {
         StringBuffer sb = new StringBuffer();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+        
+        //For each test case
         for(byte T = Byte.parseByte(br.readLine()); T > 0; --T){
+            
+            //Get input
             short N = Short.parseShort(br.readLine());
             short A = Short.parseShort(br.readLine());
             short B = Short.parseShort(br.readLine());
 
+            //Make sure A <= B
             if (A > B){
-                short temp = A;
-                A = B;
-                B = temp;
+                A ^= B;
+                B ^= A;
+                A ^= B;
             }
-
-            int val = ((int)--N)*A;
-            short dval = (short)(B - A);
-
+            
+            //Find the minimum value the last stone could be
+            int val = (N - 1) * A;
             sb.append(val);
-            if (dval > 0){
-                while(N-- > 0){
-                    sb.append(" " + (val += dval));
-                }
+            
+            //Find the other values the last stone could be in increasing order
+            if(A != B){
+                for(final short dval = (short)(B - A); --N > 0; sb.append(" " + (val += dval))){}
             }
+            
+            //Print output
             sb.append("\n");
         }
-
         System.out.print(sb);
     }
 }
